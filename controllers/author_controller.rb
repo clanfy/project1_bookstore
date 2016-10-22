2233f62e079879e57e61fb997be18afe50867108
Clacson <cookiellacson@gmail.com>
2016-10-22 19:06:03 +0100

    RR framework completed

--- controllers/author_controller.rb
+++ controllers/author_controller.rb
@@ -1,19 +1,35 @@
 #INDEX
+get '/books' do
 
+end
 
 #NEW
+get '/books/new' do
 
+end
 
 #CREATE
+post '/books' do
 
+redirect to "books"
+end
 
 #SHOW
+get '/books/:id' do
 
+end
 
 #EDIT
+get '/movies/:id/edit' do 
 
+end
 
 #UPDATE
+post '/movies/:id' do
 
+end
 
 #DELETE
+delete '/movies' do
+
+end
--- controllers/book_controller.rb
+++ controllers/book_controller.rb
@@ -1,19 +1,35 @@
 #INDEX
+get '/authors' do
 
+end
 
 #NEW
+get '/authors/new' do
 
+end
 
 #CREATE
+post '/authors' do
 
+redirect to "authors"
+end
 
 #SHOW
+get '/authors/:id' do
 
+end
 
 #EDIT
+get '/authors/:id/edit' do 
 
+end
 
 #UPDATE
+post '/authors/:id' do
 
+end
 
 #DELETE
+delete '/authors' do
+
+end
