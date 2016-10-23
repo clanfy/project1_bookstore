bafcfae3797619646d974490ff7ec23352dda5fb
Clacson <cookiellacson@gmail.com>
2016-10-23 16:43:57 +0100

    create new book started, won't update the title

--- models/book.rb
+++ models/book.rb
@@ -13,10 +13,11 @@ class Book
   end
 
   def save()
-    sql = "INSERT INTO books (title, author_id) VALUES ('#{@title}', '#{@author_id}'') RETURNING * "
+    sql = "INSERT INTO books (title, author_id) VALUES ('#{@title}', '#{@author_id}') RETURNING * "
     book_info = run_sql(sql)
     @id = book_info.first['id'].to_i
   end
+
 
   def author()
     sql = "SELECT * FROM authors WHERE id = #{@author_id}"
