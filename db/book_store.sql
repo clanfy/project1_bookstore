228f22b0bf3369eb907cb92d6eceba323740e2ae
Clacson <cookiellacson@gmail.com>
2016-10-26 12:17:39 +0100

    on delete cascade to tables to fix bug which wasn't allowing deletion of authors with books

--- db/book_store.sql
+++ db/book_store.sql
@@ -11,14 +11,14 @@ CREATE TABLE authors (
 CREATE TABLE books (
   id serial4 PRIMARY KEY,
   title VARCHAR(255),
-  author_id int4 REFERENCES authors(id),
+  author_id int4 REFERENCES authors(id) ON DELETE CASCADE,
   buy_price FLOAT,
   sell_price FLOAT
 );
 
 CREATE TABLE stock (
   id serial4 PRIMARY KEY,
-  book_id int4 REFERENCES books(id),
-  author_id int4 REFERENCES authors(id),
+  book_id int4 REFERENCES books(id) ON DELETE CASCADE,
+  author_id int4 REFERENCES authors(id) ON DELETE CASCADE,
   quantity INT2
 );
